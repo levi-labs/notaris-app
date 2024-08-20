25512b47edf1757c84899e15108fa855696362f8
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-08-20 20:42:38 +0700

    fix bug 9

--- resources/views/pages/ppat/detail.blade.php
+++ resources/views/pages/ppat/detail.blade.php
@@ -416,27 +416,7 @@
         });
         payTambahan.addEventListener('click', function() {
             // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
-            window.snap.pay('{{ $snapToken }}', {
-                onSuccess: function(result) {
-                    /* You may add your own implementation here */
-                    alert("payment success!");
-                    console.log(result);
-                },
-                onPending: function(result) {
-                    /* You may add your own implementation here */
-                    alert("wating your payment!");
-                    console.log(result);
-                },
-                onError: function(result) {
-                    /* You may add your own implementation here */
-                    alert("payment failed!");
-                    console.log(result);
-                },
-                onClose: function() {
-                    /* You may add your own implementation here */
-                    alert('you closed the popup without finishing the payment');
-                }
-            })
+            alert("payment success!");
         });
     </script>
     @include('pages.ppat.biaya-tambahan')
